7668e9ddbb255945105d66cccc95fd7c74198a1f
rcockins <ryancockins@hotmail.com>
2021-06-26 22:54:08 -0700

    commit in error

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from github import Github
 
-print("Hello World!")
-
 g = Github(" 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")
 
 
 for repo in g.get_user().get_repos():
-    print(repo.name)
+    try:
+        print(repo)
+    except(Github.GithubException):
+        print(Github.GithubException.message)
